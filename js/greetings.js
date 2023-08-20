bb5050e11650c4a4468600e48dd1ff911dc45b95
Bbangjun-Hub <140384580+Bbangjun-Hub@users.noreply.github.com>
2023-08-21 00:22:11 +0900

    Update greetings.js

--- js/greetings.js
+++ js/greetings.js
@@ -1,78 +1,3 @@
-/**const calculator = {
-    plus: function (a,b) {
-        console.log(a+b);
-    },
-    minus: function (a,b) {
-        console.log(a - b);
-    },
-    times: function (a,b) {
-        console.log(a * b);
-    },
-    divide: function (a,b) {
-        console.log(a / b);
-    },
-    power: function (a,b) {
-        console.log(a ** b);
-    },
-};**/
-
-/** const age = 96;
-function calculateKrAge(ageOfForeigner) {
-    return ageOfForeigner + 2;
-}
-
-const krAge = calculateKrAge(age);
-
-console.log(krAge); **/
-
-
-/** const h1 = document.querySelector("div.hello:first-child h1");
-
-
-function handleTitleClick() {
-    h1.classList.toggle("clicked");
-}
-
-h1.addEventListener("click", handleTitleClick); **/
-
-/** const width = window.innerWidth;
-const body = document.querySelector("body");
-const backgroundColor = body.style.backgroundColor;
-
-function colorChange() {
-  if (width < 200) {
-    backgroundColor = "cornflowerblue";
-  } else if (width >= 200 && width < 400) {
-    backgroundColor = "purple";
-  } else {
-    backgroundColor = "yellow";
-  }
-}
-
-body.addEventListener("resize",colorChange); **/
-
-/** import "./style.css";
-const body = document.body;
-const small_Screen = "smallScreen";
-const medium_Screen = "mediumScreen";
-const large_Screen = "largeScreen";
-
-function colorChange() {
-  const width = window.innerWidth;
-  if (width > 1000) {
-    body.classList.add(largeScreen);
-    body.classList.remove(mediumScreen);
-  } else if (width <= 1200 && width >= 800) {
-    body.classList.add(mediumScreen);
-    body.classList.remove(largeScreen, smallScreen);
-  } else {
-    body.classList.remove(mediumScreen);
-    body.classList.add(smallScreen);
-  }
-}
-
-window.addEventListener("resize", colorChange); **/
-
 const loginForm = document.getElementById("login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
@@ -90,7 +15,7 @@ function onloginSubmit(event) {
 
 function paintGreetings() {
   const username = localStorage.getItem(USERNAME_KEY);
-  greeting.innerText = `Today's ${username}`;
+  greeting.innerText = `You can make it, ${username}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
